2f184b21bb6d52f4a358dcdda410550142c6d5bc
Nietzsche <thomasalwyndavis@gmail.com>
2012-06-19 23:21:34 +1000

    easy

---
+++ js/views/dashboard/page.js
@@ -0,0 +1,15 @@
+define([
+  'jquery',
+  'underscore',
+  'backbone',
+  'text!templates/dashboard/page.html'
+], function($, _, Backbone, dashboardPageTemplate){
+  var DashboardPage = Backbone.View.extend({
+    el: '.page',
+    render: function () {
+		
+      $(this.el).html(dashboardPageTemplate);
+    }
+  });
+  return DashboardPage;
+});
