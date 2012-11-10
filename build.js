64329e9c6ca4f2d87f74b5440d8b7ed63ddaca0c
Thomas Davis <thomasalwyndavis@gmail.com>
2012-11-10 08:11:57 +0000

    first attempt at versiona nd files

--- build.js
+++ build.js
@@ -1,7 +1,39 @@
+RegExp.prototype.execAll = function(string) {
+    var match = null;
+    var matches = new Array();
+    while (match = this.exec(string)) {
+        var matchArray = [];
+        for (i in match) {
+            if (parseInt(i) == i) {
+                matchArray.push(match[i]);
+            }
+        }
+        matches.push(matchArray);
+    }
+    return matches;
+}
+
 var _ = require('underscore');
+var glob = require("glob")
 var fs = require('fs');
 var packages = fs.readFileSync('packages.json', 'utf8');
-var indexTemplate = fs.readFileSync('index.template', 'utf8');
+packages = JSON.parse(packages).packages;
 
-var indexPage = _.template(indexTemplate, {packages: JSON.parse(packages).packages});
+// options is optional
+glob("../cdnjs/ajax/libs/**/*.*", {}, function (er, files) {
+  var filesStr = files.join('\n');
+
+  _.each(packages, function(package) {
+    var re = new RegExp("^\.\.\/cdnjs\/ajax\/libs\/" +package.name+ "\/(.*?)\/(.*?)$","gm");
+    var packageFiles = re.execAll(filesStr);
+    package.files = packageFiles.reverse();
+  });
+  var indexTemplate = fs.readFileSync('index.template', 'utf8');
+
+var indexPage = _.template(indexTemplate, {packages: packages});
 fs.writeFileSync('index.html', indexPage, 'utf8');
+})
+
+
+
+
