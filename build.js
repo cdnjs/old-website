19c7bfa78a3578d43b6ae684d059fdaa60eb9753
Ryan Kirkman <ryan@ryankirkman.com>
2013-05-12 17:57:34 +1000

    Updated website to use html minifier

--- build.js
+++ build.js
@@ -2,12 +2,22 @@ var superagent = require('superagent');
 var _ = require('underscore');
 var fs = require('fs');
 var http = require('http');
+var htmlMinifier = require('html-minifier');
 
 superagent.get('https://s3.amazonaws.com/cdnjs-artifacts//packages.json?' + new Date().getTime(), function(res, textStatus, xhr){
   var packages = res.body.packages;
   var indexTemplate = fs.readFileSync('index.template', 'utf8');
   var indexPage = _.template(indexTemplate, {packages: packages});
-  fs.writeFileSync('index.html', indexPage, 'utf8');
+
+  var htmlCompressionOptions = {
+  	removeComments: true,
+  	collapseBooleanAttributes: true,
+  	collapseWhitespace: true,
+  };
+
+  var compressedIndexPage = htmlMinifier.minify(indexPage, htmlCompressionOptions);
+  
+  fs.writeFileSync('index.html', compressedIndexPage, 'utf8');
   fs.writeFileSync('packages.json', JSON.stringify(res.body, null, 4), 'utf8');
 });
 
