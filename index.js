1531ba45c1f505e13f62d0b62c272eb8a6b484b7
Thomas Davis <thomasalwyndavis@gmail.com>
2013-09-13 16:47:49 +1000

    reduce fileS

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+(function($){
 function selectText(element) {
     var doc = document;
     var text = element;    
@@ -19,50 +20,6 @@ $('p.library-url').on('mouseenter', function (event) {
 });
 
 
-var packages = null;
-var template = null;
-var strVar="";
-strVar += "    <% _.each(package.assets, function(asset) { %>";
-strVar += "        <tr class=\"version\">";
-strVar += "            <td class=\"versionlabel\"><%= asset.version %><\/td>";
-strVar += "        <\/tr>";
-strVar += "        <% _.each(asset.files, function(file) { %>";
-strVar += "            <tr class=\"library\">";
-strVar += "                <td>";
-strVar += "                    <p  class='library-url'>\/\/cdnjs.cloudflare.com\/ajax\/libs\/<%= package.name %>\/<%= asset.version %>\/<%=file%><\/p>";
-strVar += "                <\/td>";
-strVar += "            <\/tr>";
-strVar += "        <% }); %>";
-strVar += "    <% }); %>";
-
-$(document).ready(function(){
-    /*$(".btn-version-files").on("click", function(e){
-        e.preventDefault();
-        var package_name = $(e.currentTarget).attr('package-name');
-
-        $("#LAZY_" + package_name.replace('.', '')).html(_.template(strVar, {package: load_versions(package_name)}))
-        $(this).siblings('.filesandversions').slideToggle(200);
-        $(this).siblings('.filesandversions').find('p.library-url').on('mouseenter', function (event) {
-  selectText($(event.currentTarget)[0]);
-        });
-    });
-    load_packages(true);
-    */
-});
-
-(function($){
-    load_versions = function(package_name) {
-        load_packages(false);
-        var pkgLength = packages.length;
-        for (var i = 0; i < pkgLength; i++) {
-            if (packages[i].name == package_name){
-                return packages[i];
-            }
-        }
-    }
-})(jQuery);
-
-(function($){
   $('.search').focus();
   $('.search').on('keyup', function (ev) {
     var val = $(ev.currentTarget).val();
@@ -77,16 +34,4 @@ $(document).ready(function(){
       $('[data-library-name]').show();
     }
   });
-  load_packages = function(asyncBool) {
-    if (packages == null) {
-      $.ajax({
-        dataType: "json",
-        url: 'packages.min.json?' + new Date().getTime(),
-        async: asyncBool,
-        success: function(data){
-            packages = data.packages;
-        }
-      });
-    }
-  }
 })(jQuery);
