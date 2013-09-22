68dbbad2fc25a1bb67e6c94e58e10a34ba978d10
Lachlan Collins <lockyc@gmail.com>
2013-09-22 21:43:30 +1000

    Updated packages.

--- build.sh
+++ build.sh
@@ -2,15 +2,16 @@
 # Exit if any errors
 set -e
 
-echo npm install for good mesure
-npm install
-
 echo Getting latest website changes...
 git pull
+
+echo npm install for good mesure
+npm install
 
 echo Re-generating website...
 node build.js
 
 echo Uploading website...
+git add
 git commit -am "Updated packages."
 git push
