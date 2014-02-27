fee72779436cdee987cd233dda37624ede848636
Thomas Davis <thomasalwyndavis@gmail.com>
2014-02-27 19:43:59 +1000

    express

--- api.js
+++ api.js
@@ -4,7 +4,19 @@ var fs = require('fs');
 var express = require('express');
 var app = express();
 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
+	next();
+}
+
+app.use(express.bodyParser());
+app.use(allowCrossDomain);
+
 app.get('/', function(req, res){
+res.setHeader("Expires", new Date(Date.now() + 5 * 60 * 1000).toUTCString());
   res.send('hello world');
 });
 var port = process.env.PORT || 5000;
