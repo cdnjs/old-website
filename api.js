a1c8f8bd82072264a122db1c029e23e6f4a27285
Thomas Davis <thomasalwyndavis@gmail.com>
2014-02-27 19:40:05 +1000

    added api server

--- api.js
+++ api.js
@@ -1,3 +1,12 @@
 var fs = require('fs');
 
-console.log('a')
+
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res){
+  res.send('hello world');
+});
+var port = process.env.PORT || 5000;
+
+app.listen(port);
